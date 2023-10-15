98ba9a5b6b8085592551bf4d8401ae6600f83fac
Fanny <fsb62@hotmail.com>
2023-10-15 21:17:06 +0200

    sccript

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('weather app');
+
+const apiKey = '78d27b9c42d24cab4711a11603dd0d0f';
+const tempElement = document.getElementById('temp');
+const sunriseElement = document.getElementById('sunrise');
+const sunsetElement = document.getElementById('sunset');
+const cityElement = document.getElementById('city');
+const todayWeatherIconContainer = document.querySelector('.today-weather-icon');
+const forecastContainer = document.getElementById('forecast-list');
